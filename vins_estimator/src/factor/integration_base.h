3e3c2ba792922fbc3320a2839177ce90306b73d0
gyyc233_bot <1056616678@qq.com>
2025-08-24 17:10:17 +0800

    review integration_base

--- vins_estimator/src/factor/integration_base.h
+++ vins_estimator/src/factor/integration_base.h
@@ -6,10 +6,17 @@
 #include <ceres/ceres.h>
 using namespace Eigen;
 
+/// @brief IMU预积分类 ：加速度计、陀螺仪、线性加速度计ba、陀螺仪bg、雅克比矩阵初始化、协方差矩阵15*15、dt、PVQ
 class IntegrationBase
 {
   public:
     IntegrationBase() = delete;
+
+    /// @brief 
+    /// @param _acc_0 初始时刻的加速度
+    /// @param _gyr_0 初始时刻的角速度
+    /// @param _linearized_ba 初始时刻加速计的零偏
+    /// @param _linearized_bg 初始时刻角速度计的零偏
     IntegrationBase(const Eigen::Vector3d &_acc_0, const Eigen::Vector3d &_gyr_0,
                     const Eigen::Vector3d &_linearized_ba, const Eigen::Vector3d &_linearized_bg)
         : acc_0{_acc_0}, gyr_0{_gyr_0}, linearized_acc{_acc_0}, linearized_gyr{_gyr_0},
@@ -18,13 +25,15 @@ class IntegrationBase
           sum_dt{0.0}, delta_p{Eigen::Vector3d::Zero()}, delta_q{Eigen::Quaterniond::Identity()}, delta_v{Eigen::Vector3d::Zero()}
 
     {
+        // ΔXk+1 = F * ΔXk + G * nk ,这里的噪声项nk为18*1的矩阵
+        // 则ΔXk+1的协方差矩阵为 Pk+1 = F*Pk*F^T + G*V*G^T, 这里噪声的协方差矩阵为18*18的矩阵
         noise = Eigen::Matrix<double, 18, 18>::Zero();
-        noise.block<3, 3>(0, 0) =  (ACC_N * ACC_N) * Eigen::Matrix3d::Identity();
-        noise.block<3, 3>(3, 3) =  (GYR_N * GYR_N) * Eigen::Matrix3d::Identity();
+        noise.block<3, 3>(0, 0) =  (ACC_N * ACC_N) * Eigen::Matrix3d::Identity(); // k时刻k+1时刻加速度计的噪声是相同的
+        noise.block<3, 3>(3, 3) =  (GYR_N * GYR_N) * Eigen::Matrix3d::Identity(); // k时刻k+1时刻陀螺仪的噪声是相同的
         noise.block<3, 3>(6, 6) =  (ACC_N * ACC_N) * Eigen::Matrix3d::Identity();
         noise.block<3, 3>(9, 9) =  (GYR_N * GYR_N) * Eigen::Matrix3d::Identity();
-        noise.block<3, 3>(12, 12) =  (ACC_W * ACC_W) * Eigen::Matrix3d::Identity();
-        noise.block<3, 3>(15, 15) =  (GYR_W * GYR_W) * Eigen::Matrix3d::Identity();
+        noise.block<3, 3>(12, 12) =  (ACC_W * ACC_W) * Eigen::Matrix3d::Identity(); // 加速度计零偏的噪声
+        noise.block<3, 3>(15, 15) =  (GYR_W * GYR_W) * Eigen::Matrix3d::Identity(); // 陀螺仪零偏的噪声
     }
 
     void push_back(double dt, const Eigen::Vector3d &acc, const Eigen::Vector3d &gyr)
@@ -32,9 +41,13 @@ class IntegrationBase
         dt_buf.push_back(dt);
         acc_buf.push_back(acc);
         gyr_buf.push_back(gyr);
+        // IMU预积分传播方程
         propagate(dt, acc, gyr);
     }
 
+    /// @brief 优化过程中Bias会更新，需要根据新的bias重新计算预积分
+    /// @param _linearized_ba 
+    /// @param _linearized_bg 
     void repropagate(const Eigen::Vector3d &_linearized_ba, const Eigen::Vector3d &_linearized_bg)
     {
         sum_dt = 0.0;
@@ -51,6 +64,7 @@ class IntegrationBase
             propagate(dt_buf[i], acc_buf[i], gyr_buf[i]);
     }
 
+    /// @brief IMU预积分中采用中值积分递推Jacobian和Covariance，构造误差的线性化递推方程，得到Jacobian和Covariance递推公式-> Paper 式27
     void midPointIntegration(double _dt, 
                             const Eigen::Vector3d &_acc_0, const Eigen::Vector3d &_gyr_0,
                             const Eigen::Vector3d &_acc_1, const Eigen::Vector3d &_gyr_1,
@@ -60,8 +74,12 @@ class IntegrationBase
                             Eigen::Vector3d &result_linearized_ba, Eigen::Vector3d &result_linearized_bg, bool update_jacobian)
     {
         //ROS_INFO("midpoint integration");
+        // step 1: 中值积分更新状态量
+        // a = R(γ)(ai - ba)
         Vector3d un_acc_0 = delta_q * (_acc_0 - linearized_ba);
+        // ω = 1/2(ω0 + ω1) - bw
         Vector3d un_gyr = 0.5 * (_gyr_0 + _gyr_1) - linearized_bg;
+        // 四元数转化为轴角：q = [cosθ/2,n * sinθ/2] ,当θ趋近于0时： θ -> 0 => q = [1,w*t/2]
         result_delta_q = delta_q * Quaterniond(1, un_gyr(0) * _dt / 2, un_gyr(1) * _dt / 2, un_gyr(2) * _dt / 2);
         Vector3d un_acc_1 = result_delta_q * (_acc_1 - linearized_ba);
         Vector3d un_acc = 0.5 * (un_acc_0 + un_acc_1);
@@ -70,6 +88,7 @@ class IntegrationBase
         result_linearized_ba = linearized_ba;
         result_linearized_bg = linearized_bg;         
 
+        // 更新方差矩阵与Jacobian
         if(update_jacobian)
         {
             Vector3d w_x = 0.5 * (_gyr_0 + _gyr_1) - linearized_bg;
@@ -86,6 +105,15 @@ class IntegrationBase
             R_a_1_x<<0, -a_1_x(2), a_1_x(1),
                 a_1_x(2), 0, -a_1_x(0),
                 -a_1_x(1), a_1_x(0), 0;
+
+            // 协方差矩阵 δx_k+1 = F * δ_k + V * n_t
+            /*[ δα_k+1  ]   [ I f01 δt f03 f04 ] [ δα_k  ]   [ v00        v01      v02      v03  0  0 ][ n_ak  ]
+             *[ δθ_k+1  ]   [ 0 f11  0   0 -δt ] [ δθ_k  ]   [  0       -δt/2        0    -δt/2  0  0 ][ n_ωk  ]
+             *[ δβ_k+1  ] = [ 0 f21  I f23 f24 ] [ δβ_k  ] + [ -R_k*δt/2  v21  -R_k+1*δt/2  v23  0  0 ][ n_ak+1]
+             *[ δb_a_k+1]   [ 0   0  0   0   0 ] [ δb_a_k]   [  0          0         0       0   0  0 ][ n_ωk+1]
+             *[ δb_ω_k+1]   [ 0   0  0   0   0 ] [ δb_ω_k]   [  0          0         0       0   0  δt][ n_ba  ]
+             *                                                                                         [ n_bw  ]
+             */
 
             MatrixXd F = MatrixXd::Zero(15, 15);
             F.block<3, 3>(0, 0) = Matrix3d::Identity();
@@ -121,12 +149,19 @@ class IntegrationBase
 
             //step_jacobian = F;
             //step_V = V;
+            // 这里的F 作用是对jacobian矩阵的信息矩阵
+            // F V 作用是协方差矩阵的信息矩阵
             jacobian = F * jacobian;
             covariance = F * covariance * F.transpose() + V * noise * V.transpose();
         }
 
     }
 
+    /// @brief IMU预积分传播方程
+    /// @details 积分计算两个关键帧之间IMU测量的变化量：旋转delta_q 速度delta_v 位移delta_p, 加速度的bias linearized_ba 陀螺仪的Bias linearized_bg, 同时维护更新预积分的Jacobian和Covariance,计算优化时必要的参数
+    /// @param _dt 
+    /// @param _acc_1 
+    /// @param _gyr_1 
     void propagate(double _dt, const Eigen::Vector3d &_acc_1, const Eigen::Vector3d &_gyr_1)
     {
         dt = _dt;
@@ -157,11 +192,14 @@ class IntegrationBase
      
     }
 
+    /// @brief 计算和给定相邻帧状态量的残差
+    /// @details Pi，Qi，Vi，Bai，Bgi  [前一次预积分结果]; Pj，Qj，Vj，Baj，Bgj  [后一次预积分结果]
     Eigen::Matrix<double, 15, 1> evaluate(const Eigen::Vector3d &Pi, const Eigen::Quaterniond &Qi, const Eigen::Vector3d &Vi, const Eigen::Vector3d &Bai, const Eigen::Vector3d &Bgi,
                                           const Eigen::Vector3d &Pj, const Eigen::Quaterniond &Qj, const Eigen::Vector3d &Vj, const Eigen::Vector3d &Baj, const Eigen::Vector3d &Bgj)
     {
         Eigen::Matrix<double, 15, 1> residuals;
 
+        // 获取P V Q 关于 ba，bg的雅克比
         Eigen::Matrix3d dp_dba = jacobian.block<3, 3>(O_P, O_BA);
         Eigen::Matrix3d dp_dbg = jacobian.block<3, 3>(O_P, O_BG);
 
@@ -173,13 +211,20 @@ class IntegrationBase
         Eigen::Vector3d dba = Bai - linearized_ba;
         Eigen::Vector3d dbg = Bgi - linearized_bg;
 
+        // 计算bias改变后的旋转，速度，位移预积分观测值
         Eigen::Quaterniond corrected_delta_q = delta_q * Utility::deltaQ(dq_dbg * dbg);
         Eigen::Vector3d corrected_delta_v = delta_v + dv_dba * dba + dv_dbg * dbg;
         Eigen::Vector3d corrected_delta_p = delta_p + dp_dba * dba + dp_dbg * dbg;
 
+        // 计算imu残差
+
+        // 位置残差
         residuals.block<3, 1>(O_P, 0) = Qi.inverse() * (0.5 * G * sum_dt * sum_dt + Pj - Pi - Vi * sum_dt) - corrected_delta_p;
+        // 旋转残差，.vec()：取出虚部
         residuals.block<3, 1>(O_R, 0) = 2 * (corrected_delta_q.inverse() * (Qi.inverse() * Qj)).vec();
+        // 速度残差
         residuals.block<3, 1>(O_V, 0) = Qi.inverse() * (G * sum_dt + Vj - Vi) - corrected_delta_v;
+        // 零偏
         residuals.block<3, 1>(O_BA, 0) = Baj - Bai;
         residuals.block<3, 1>(O_BG, 0) = Bgj - Bgi;
         return residuals;
